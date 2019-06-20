e264bbe0a246b4f63968534826f03347bdc7f708
Max Lee <maxolee23@gmail.com>
2019-06-20 18:53:06 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -60,3 +60,16 @@ describe('sayHiToGrandma(string)', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
   })
 })
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
